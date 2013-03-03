b58013ba63caf0dbe5e5af733b0554797b3c55cc
Alex Bain <alex@alexba.in>
2013-03-02 17:11:39 -0800

    Initial pass at home page.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+$(window).load(function() {
+
+    setTimeout(function() {
+        $('body').addClass('in');
+
+        setTimeout(function() {
+            $('.hero-unit .fade').addClass('in');
+
+            setTimeout(function() {
+                $('.getting-started-container').addClass('in');
+                $('.footer').addClass('in');
+            }, 750);
+
+        }, 750);
+
+    }, 250);
+
+});
