5e723d39633fb6473af3786dd0054575ec304f6e
Alex Bain <alex@alexba.in>
2013-03-02 17:17:00 -0800

    Adding optimized dist subtree.

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+$(window).load(function(){setTimeout(function(){$("body").addClass("in"),setTimeout(function(){$(".hero-unit .fade").addClass("in"),setTimeout(function(){$(".getting-started-container").addClass("in"),$(".footer").addClass("in")},750)},750)},250)});
