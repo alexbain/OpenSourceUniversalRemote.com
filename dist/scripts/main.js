ea190acae3fd81371c99cacd756574b1c6d774cd
Alex Bain <alex@alexba.in>
2013-03-05 13:49:39 -0800

    Overhauling UI for presentation.

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -9604,6 +9604,7 @@ $(window).load(function() {
             $('.hero-unit .fade').addClass('in');
 
             setTimeout(function() {
+                $('.quick-overview-container').addClass('in');
                 $('.getting-started-container').addClass('in');
                 $('.footer').addClass('in');
             }, 750);
